490d2070369e165101623ffb2578e9c5d12194dd
Azure Developer <azure@example.com>
2026-05-06 09:16:51 -0400

    Initial commit: Azure Static Web Apps with React, TypeScript, and Azure Functions - Complete starter template with authentication, authorization, and CI/CD

---
+++ xfdl Parser/xfdl Parser/XDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XfdlParser
+{
+    public class XDocument
+    {
+        internal IEnumerable<object> Descendants()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
